2cdc3c8094c0c31524d078d1076b03d00a7f921a
avinashwele <60333815+avinashwele@users.noreply.github.com>
2026-02-11 14:24:09 +0530

    Initial project structure

---
+++ backend/src/main/java/com/studigma/backend/StudigmaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.studigma.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudigmaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudigmaBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/studigma/backend/StudigmaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studigma.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudigmaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
